f86c80017c68f0362f00d09024fa52ff0410aa66
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ JS/dxSample_Mobile/DxSample/views/index.js
@@ -0,0 +1,20 @@
+﻿DxSample.Index = function (params) {
+    var viewModel = {
+        autocompleteData: [
+            "Afghanistan",
+            "Akrotiri",
+            "Albania",
+            "Algeria",
+            "American Samoa",
+            "Andorra",
+            "Angola"
+        ],
+        maxlength: ko.observable(5),
+        placeholder: ko.observable("Max length is set to "),
+        viewRendered: function () {
+            $('#autocompleteContainer input').attr('placeholder', viewModel.placeholder() + viewModel.maxlength());
+            $('#autocompleteContainer input').attr('maxlength', viewModel.maxlength());
+        }
+    };
+    return viewModel;
+};
